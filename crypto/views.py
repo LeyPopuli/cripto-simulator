33371a639d37a166a652c35b6a6f202df9cee918
LeyPopuli <leyrezgz95@gmail.com>
2023-03-23 12:55:30 +0100

    Navbar with dynamic links finished

--- crypto/views.py
+++ crypto/views.py
@@ -4,6 +4,25 @@ from . import app
 
 
 @app.route('/')
-def home():
-    return render_template('base.html')
+def index():
+    return render_template('index.html')
 
+
+@app.route('/purchase')
+def purchase():
+    return render_template('purchase.html')
+
+
+@app.route('/status')
+def status():
+    return render_template('status.html')
+
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
